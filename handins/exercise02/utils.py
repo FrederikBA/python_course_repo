58bb37195888df8fec72cb41f5fc99dc30e5d35d
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2022-02-11 21:18:34 +0100

    exercise 2 2/5

--- handins/exercise02/utils.py
+++ handins/exercise02/utils.py
@@ -1,7 +1,20 @@
 import argparse
+import os
 
-def get_file_names(folderpath, out=output.txt):
+def get_file_names(folderpath ,out='output.txt'):
+  with open(out, 'w') as file_object:
+    for f in os.listdir(folderpath):
+      file_object.write(f + '\n')
+      
+      
+      
+def get_all_file_names(folderpath,out='output.txt'):
+  with open(out, 'w') as file_object:
+      for (root, dirs, files) in os.walk(folderpath):
+        file_object.write(''.join(files) + '\n')
+      
   
-
 if __name__ == '__main__':
+  #get_file_names('/home/jovyan/my_notebooks')
+  #get_all_file_names('/home/jovyan/my_notebooks')
   print('Hello World')
