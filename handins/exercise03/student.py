e723f9e960c5cfb00cdd374314ccfd8af4f01ff7
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2022-02-21 14:04:25 +0100

    initiate handin

--- handins/exercise03/student.py
+++ handins/exercise03/student.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Student:
     def __init__(self, name, gender, data_sheet, image_url):
         self.name = name
@@ -8,6 +11,12 @@ class Student:
     def get_avg_grade(self):
         grades = self.data_sheet.get_grades_as_list()
         return sum(grades) / len(grades)
+    
+    def show_progession(self):
+        max_value = len(self.data_sheet.courses) * 150
+        sum_ects = sum(self.data_sheet.get_ects())
+        return sum_ects  / max_value * 100
+    
 
 
 class Course:
@@ -17,6 +26,10 @@ class Course:
         self.teacher = teacher
         self.ects = ects
         self.grade = grade
+
+    def set_grade(self):
+        grades = [0, 2, 4, 7, 10, 12]
+        self.grade = random.choice(grades)
 
 
 class DataSheet:
@@ -33,29 +46,69 @@ class DataSheet:
             self.courses.append(new_course)
 
     def get_grades_as_list(self):
-        return [course.grade for course in self.courses if course.grade is not None]
+        return [course.grade for course in self.courses if course.grade != None]
+
+    def get_ects(self):
+        return [course.ects for course in self.courses]
+
+
+def generate_students(amount, name_list, courses, img_url_list):
+    genders = ["Male", "Female"]
+    index = 0
+    students = []
+    while index <= amount:
+        randCourseArray = []
+        randCourse = random.choice(courses)
+        randCourse.set_grade()
+        randCourseArray.append(randCourse)
+        currentStud = Student(
+            random.choice(name_list),
+            random.choice(genders),
+            DataSheet(randCourseArray),
+            random.choice(img_url_list),
+        )
+        students.append(currentStud)
+        index += 1
+    return students
 
 
 course_list = []
 
-course_list.append(Course("Python", "105", "Thomas", 130, 4))
-course_list.append(Course("Game Development", "105", "Jesper", 130, 12))
-course_list.append(Course("OOA/OOD", "105", "Kim", 130, 10))
+course_list.append(Course("Python", "105", "Thomas", 150, 4))
+course_list.append(Course("Game Development", "105", "Jesper", 150, 12))
+course_list.append(Course("OOA/OOD", "105", "Kim", 150, 10))
 
 d1 = DataSheet(course_list)
-d2 = DataSheet(
-    [
-        Course("Python", "105", "Thomas", 130, 4),
-        Course("Game Development", "105", "Jesper", 130, 12),
-        Course("OOA/OOD", "105", "Kim", 130, 10),
-    ]
-)
 
 s1 = Student("Janus", "Male", d1, "www.image.com")
 s2 = Student("Rasmus", "Male", d1, "www.image.com")
 s3 = Student("Frederik", "Male", d1, "www.image.com")
 
-print(d1.get_grades_as_list())
-print(s2.get_avg_grade())
 
-print(d2.courses[0].name)
+names = [
+    "Bob",
+    "Lise",
+    "Karen",
+    "Rasmus",
+    "Jon",
+    "Jørgen",
+    "Viggo",
+    "Merete",
+    "Inge-Lise",
+    "Ingvar",
+    "Johnny",
+]
+
+url_list = []
+url_list.append("www.erdetfredag.dk")
+url_list.append("www.arla.dk")
+url_list.append("www.dr.dk")
+url_list.append("www.youtube.com")
+url_list.append("www.digitalocean.com")
+
+
+# print(generate_students(5, names, course_list, url_list))
+#print(d1.get_grades_as_list())
+# print(s2.get_avg_grade())
+#print(d1.get_ects())
+#print(s1.show_progession())
